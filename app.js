4fe0218f68bf706796fd5fa90bf0a736905f6a53
Pravallika Mummadi <s555592@nwmissouri.edu>
2023-03-29 17:14:49 -0500

    bonus

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
+var computationRouter = require('./routes/computation');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/mydata', mydataRouter);
+app.use('/computation', computationRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
