e7f6fd006cead045e468b5e85c61ca2c759c1a72
Pravallika Mummadi <s555592@nwmissouri.edu>
2023-03-29 16:31:29 -0500

    bonus

---
+++ routes/computation.js
@@ -0,0 +1,48 @@
+var express = require('express');
+
+var router = express.Router();
+
+
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+
+    var p1=Math.floor(Math.random()*15)
+
+    var p2=Math.floor(Math.random()*15)
+
+    var p3=Math.floor(Math.random()*15)
+
+    var atan=Math.atan2(p1)
+
+    var atanh=Math.atanh(p2)
+
+    var cbrt=Math.cbrt(p3)
+
+    res.render('computation',
+
+    {
+
+        title:'Pravallika Mummadi Computes Functions',
+
+        p1:p1,
+
+        p2:p2,
+
+        p3:p3,
+
+        p:atan,
+        
+        q:atanh,
+
+        r:cbrt,
+
+       
+    });
+
+     
+
+});
+
+module.exports=router;
