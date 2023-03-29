1a0a1ec83ddc462ba623ce714c7a47695eae760f
Pravallika Mummadi <s555592@nwmissouri.edu>
2023-03-29 15:14:32 -0500

    edited

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var sum=0;
-var more=0;
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  more +=1;
-  sum +=more;
-  res.send('Sum is :'+sum);
-  
+  res.render('mydata', { title: 'Pravallika Mummadi' });
 });
 
 module.exports = router;
